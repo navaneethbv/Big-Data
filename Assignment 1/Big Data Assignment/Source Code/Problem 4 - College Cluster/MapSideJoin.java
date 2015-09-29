12701c60b7ad721e0a8157cac9bd050de6b64772
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2015-09-29 11:00:39 -0500

    Update MapSideJoin.java

--- Assignment 1/Big Data Assignment/Source Code/Problem 4 - College Cluster/MapSideJoin.java
+++ Assignment 1/Big Data Assignment/Source Code/Problem 4 - College Cluster/MapSideJoin.java
@@ -13,7 +13,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Job; 
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
