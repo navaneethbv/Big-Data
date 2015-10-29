161488371a4bf8404a6014cd25dfe0436f493fcd
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2015-10-29 01:23:04 -0500

    Update Assignment2_2.scala

--- Assignment_2/Source Code/Assignment2_2.scala
+++ Assignment_2/Source Code/Assignment2_2.scala
@@ -5,7 +5,7 @@
 -- spark-shell --master yarn-client --executor-memory 4G --executor-cores 7 --num-executors 6
 
 --Code
-val reviewFile = "/yelpdatafall/review/review.csv"
+val reviewFile = "/yelpdatafall/review/review.csv" 
 val reviewData = sc.textFile(reviewFile, 2)
 val mapReviewData = reviewData.map(line => line.split("\\^")).map(word => (word(2),(word(3).toDouble,1.0)))
 val avgReviewData = mapReviewData.reduceByKey((x,y) => ((x._1+y._1)/(x._2+y._2),1.0)).map{case(key,value) => (key,value._1.toInt)}
