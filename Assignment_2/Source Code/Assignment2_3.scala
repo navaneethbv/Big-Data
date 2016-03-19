a16ca1dfe3a92b6baa512e2986f891c060c53a94
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2016-03-19 13:01:49 -0500

    Update Assignment2_3.scala

--- Assignment_2/Source Code/Assignment2_3.scala
+++ Assignment_2/Source Code/Assignment2_3.scala
@@ -1,4 +1,4 @@
--- 3 A))
+-- 3 A)) 
 val data = sc.textFile("/yelpdatafall/review/review.csv")
 val data1 = data.map(line => line.split("\\^")).map(line => (line(2), line(3).toDouble))
 val result =  data1.map { case (key, value) => (key, (value, 1)) }.reduceByKey { case ((value1, count1), (value2, count2)) => (value1 + value2, count1 + count2)}.mapValues {case (value, count) =>  value.toDouble / count.toDouble}
