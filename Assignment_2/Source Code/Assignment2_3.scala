e192ccdc472a0294f03e657cf211e2c804d5c2cf
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2016-03-19 13:02:09 -0500

    Update Assignment2_3.scala

--- Assignment_2/Source Code/Assignment2_3.scala
+++ Assignment_2/Source Code/Assignment2_3.scala
@@ -4,7 +4,7 @@ val data1 = data.map(line => line.split("\\^")).map(line => (line(2), line(3).to
 val result =  data1.map { case (key, value) => (key, (value, 1)) }.reduceByKey { case ((value1, count1), (value2, count2)) => (value1 + value2, count1 + count2)}.mapValues {case (value, count) =>  value.toDouble / count.toDouble}
 val topTen = result.takeOrdered(10)(Ordering[Double].reverse.on(x=>x._2)) 
 val topTen2 = sc.parallelize(topTen) 
-
+ 
 val data2 = sc.textFile("/yelpdatafall/business/business.csv")
 val data3 = data2.map(line => line.split("\\^")).map(line => (line(0), (line(1)+"\t\t"+line(2)).toString)) 
 
