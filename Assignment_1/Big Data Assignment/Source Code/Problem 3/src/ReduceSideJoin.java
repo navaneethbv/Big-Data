9164916f33e3df9ff4436c2e912f92e52310c921
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2016-03-21 23:28:19 -0500

    Update ReduceSideJoin.java

--- Assignment_1/Big Data Assignment/Source Code/Problem 3/src/ReduceSideJoin.java
+++ Assignment_1/Big Data Assignment/Source Code/Problem 3/src/ReduceSideJoin.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.TreeMap;   
 import java.util.regex.Pattern;
  
-import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configuration; 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.FloatWritable; 
 import org.apache.hadoop.io.LongWritable;
