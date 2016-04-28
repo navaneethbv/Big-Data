b6ff3b8ebc7cd6bf4f2b6350085ac1696738d127
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2016-04-28 11:21:50 -0500

    Update Assignment2_2.scala

--- Assignment_2/Source Code/Assignment2_2.scala
+++ Assignment_2/Source Code/Assignment2_2.scala
@@ -1,7 +1,7 @@
 -- To start Stardalone Standalone mode
 -- spark-shell --master local[*]
 -- to start Yarn mode
--- spark-shell --master yarn-client --executor-memory 4G --executor-cores 7 --num-executors 6 
+-- spark-shell --master yarn-client --executor-memory 4G --executor-cores 7 --num-executors 6  
 
 --Code 
 val reviewFile = "/yelpdatafall/review/review.csv" 
