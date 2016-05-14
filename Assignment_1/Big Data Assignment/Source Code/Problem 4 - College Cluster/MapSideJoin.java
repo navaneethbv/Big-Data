b5600bd6491ed74d172b66e1696aa9c47da4b78b
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2016-05-14 18:38:31 -0500

    Update MapSideJoin.java

--- Assignment_1/Big Data Assignment/Source Code/Problem 4 - College Cluster/MapSideJoin.java
+++ Assignment_1/Big Data Assignment/Source Code/Problem 4 - College Cluster/MapSideJoin.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStreamReader; 
+
 import java.net.URISyntaxException;
+
 import java.util.Comparator;
 import java.util.HashMap; 
 import java.util.Map; 
